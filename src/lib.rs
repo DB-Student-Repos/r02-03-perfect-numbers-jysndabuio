77e0c3e55419d713dc661207839611f767491f1c
jysndabuio <jysndabu@outlook.com>
2024-03-22 14:41:11 +0100

    Solution

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,26 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {num}");
+
+    if num == 0 {
+        return None;
+    }
+
+    let mut factors: Vec<u64> = Vec::new();
+
+    for x in 1..num {
+        if num % x == 0 {
+            factors.push(x);
+        }
+    }
+
+    let total:u64 = factors.iter().sum();
+
+    Some(match total {
+        total if total == num => Classification::Perfect,
+        total if total > num => Classification::Abundant,
+        total if total < num => Classification::Deficient,
+        _ => Classification::Deficient, // With error if not use, to handle remaining cases.
+    })
+
 }
